c8a114ce576e1eac4adc90dd07512dd0aa505b06
Zephery <1570631036@qq.com>
2018-01-10 23:40:13 +0800

    update

---
+++ crawl-lucene/src/main/java/com/crawl/lucene/crawllucene/CrawlLuceneApplication.java
@@ -0,0 +1,12 @@
+package com.crawl.lucene.crawllucene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlLuceneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlLuceneApplication.class, args);
+	}
+}
